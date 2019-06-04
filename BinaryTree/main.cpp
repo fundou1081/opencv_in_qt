5c0fbb41327614442b2e83e2e7c537ed89d0cfc9
fundou1081 <fundou1081@outlook.com>
2019-06-04 15:31:40 +0800

    20190604

---
+++ BinaryTree/main.cpp
@@ -0,0 +1,277 @@
+﻿#include <iostream>
+#include <random>
+#include <ctime>
+#include <stack>
+
+#define aSize 60000
+using namespace std;
+
+
+void showResult(int a[])
+{
+    for(int i=0;i<aSize;i++)
+    {
+        cout << a[i]<<' ';
+    }
+    cout <<endl;
+}
+
+void popsort(int a[]){
+    int s=0;
+    for(int i=0;i<aSize;i++)
+    {
+        for(int j=i;j<aSize-1;j++)
+        {
+            if(a[j] < a[j+1])
+            {
+                int tmp =a[j+1];
+                a[j+1]=a[j];
+                a[j] = tmp;
+                s++;
+            }
+        }
+    }
+    cout<<"s="<<s<<endl;
+}
+
+void popsort2(int a[]){
+    int s=0;
+    for(int i=0;i<aSize;i++)
+    {
+        for(int j=i;j<aSize-1;j++)
+        {
+            if(a[j] < a[j+1])
+            {
+                int tmp =a[j+1];
+                a[j+1]=a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+    cout<<"s="<<s<<endl;
+}
+
+void selectsort(int a[])
+{
+    int s=0;
+    for(int i=0;i<aSize-1;i++){
+        int max=i;
+        for(int j=i+1;j<aSize;j++)
+        {
+            if(a[max] < a[j]){
+                max = j;
+                s++;
+           }
+        }
+        int tmp = a[i];
+        a[i]=a[max];
+        a[max] = tmp;
+    }
+    cout<<"s="<<s<<endl;
+}
+
+void selectsort2(int a[])
+{
+    int s=0;
+    for(int i=0;i<aSize;i++){
+        int max=i;
+        for(int j=aSize-1;j>i;j--)
+        {
+            if(a[max] < a[j]){
+                 max = j;
+                 s++;
+            }
+        }
+        int tmp = a[i];
+        a[i]=a[max];
+        a[max] = tmp;
+
+    }
+    cout<<"s="<<s<<endl;
+}
+
+void qsort(int a[],int l,int r){
+
+    if(l<r){
+        int i=l;
+        int j=r;
+        int x=a[l];
+
+        while(i<j)
+        {
+            while(a[j]>x && i<j)   j--;
+            if(i<j){
+                a[i]=a[j];
+                i++;
+                //showResult(a);
+            }
+
+            while(a[i]<x && i<j)   i++;
+            if(i<j){
+                a[j]=a[i];
+                j--;
+                //showResult(a);
+            }
+        }
+        a[i]=x;
+        //cout <<"@@@ ";
+        //showResult(a);
+        qsort(a,l,i-1);
+        qsort(a,i+1,r);
+    }
+}
+
+void qsort2(int a[]){
+    stack<int> si;
+    stack<int> sj;
+    stack<int> smdata;
+
+    int i=0;
+    int j=0;
+    int mdata;
+
+    si.push(0);
+    sj.push(aSize-1);
+    smdata.push(a[i]);
+
+    int af,bf;
+
+    while(!(si.empty() && sj.empty() ))
+    {
+        i=si.top();
+        j=sj.top();
+        mdata=smdata.top();
+
+        si.pop();
+        sj.pop();
+        smdata.pop();
+
+        af=i;bf=j;
+
+        if (af==bf) continue;
+
+        mdata = a[i];
+        while(i<j)
+        {
+            while(i<j && mdata < a[j]){
+                j--;
+            }
+            if(i<j){
+                a[i]=a[j];
+                i++;
+            }
+            while(i<j && a[i] < mdata){
+                i++;
+            }
+            if(i<j){
+                a[j]=a[i];
+                j--;
+            }
+        }
+        a[i]=mdata;
+
+        // B
+        if(i+1<bf){
+            si.push(i+1);
+            sj.push(bf);
+            smdata.push(a[i+1]);
+        }
+
+        // A
+        if(af<i-1){
+            si.push(af);
+            sj.push(i-1);
+            smdata.push(a[af]);
+        }
+
+        showResult(a);
+    }
+
+}
+
+void mergeA(int a[], int first, int mid, int last, int temp[]){
+    int i = first, j = mid + 1;
+    int m = mid,   n = last;
+    int k = 0;
+    while (i <= m && j <= n)
+    {
+        if (a[i] <= a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
+    }
+
+    while (i <= m)
+        temp[k++] = a[i++];
+
+    while (j <= n)
+        temp[k++] = a[j++];
+
+    for (i = 0; i < k; i++)
+        a[first + i] = temp[i];
+
+}
+
+void merge(int a[],int first, int last, int p[]){
+    if(first < last)
+    {
+        int mid = (first + last) / 2;
+        merge(a,first,mid,p);
+        merge(a,mid+1,last,p);
+        mergeA(a,first,mid,last,p);
+    }
+}
+
+
+void Mergesort(int a[]){
+    int* p =new int[aSize];
+    merge(a,0,aSize-1,p);
+    delete[] p;
+}
+
+
+int main()
+{
+    default_random_engine e(211);
+    uniform_int_distribution<int> u(0,100);
+
+    clock_t start,finish;
+
+
+    int a[aSize];
+    int b[aSize];
+    for(int i=0;i<aSize;i++)
+    {
+        a[i]=u(e);
+        b[i]=a[i];
+    }
+
+    showResult(a);
+
+    //
+    start = clock();
+    //popsort(a);
+    //selectsort(a);
+    //selectsort2(b);
+    qsort(a,0,aSize-1);
+    //qsort2(a);
+    Mergesort(b);
+    finish = clock();
+    //
+    int timeA = finish-start;
+
+    //
+    start = clock();
+    Mergesort(b);
+    finish = clock();
+    //
+    int timeB = finish-start;
+
+    cout << "OK" <<endl;
+
+    showResult(a);
+    cout << "timeA=" << timeA << endl;
+    cout << "timeB=" << timeB << endl;
+
+    return 0;
+}
---
+++ ImgHist/main.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+void histone(Mat& image);
+
+
+int main(int argc, char *argv[])
+{
+    Mat image = imread("D:\\ttest.jpg");
+
+    Mat dimage;
+    Mat hsvimage;
+
+    if(!image.data){
+        cout<<"error"<<endl;return -1;
+    }
+
+    cvtColor(image,hsvimage,COLOR_BGR2HSV);
+
+    int hueBinNum =30;
+    int saturationBinNum    =   32;
+    int histSize[]={hueBinNum,saturationBinNum};
+
+    float hueRange[]={0,180};
+
+    float saturationRange[] = {0,256};
+
+    const float* ranges[]={hueRange,saturationRange};
+
+    MatND   dstHist;
+    int channels[]={0,1};
+
+    calcHist(&hsvimage,
+             1,
+             channels,
+             Mat(),
+             dstHist,
+             2,
+             histSize,
+             ranges,true,false);
+
+    double maxValue =0;
+    minMaxLoc(dstHist,0,&maxValue,0,0);
+
+    int scale=10;
+
+    Mat histImg = Mat::zeros(saturationBinNum*scale,hueBinNum*10,CV_8UC3);
+
+    for(int hue=0;hue<hueBinNum;hue++)
+        for(int saturation=0;saturation<saturationBinNum;saturation++)
+        {
+            float binValue = dstHist.at<float>(hue,saturation);
+            int intensity = cvRound(binValue*255/maxValue);
+
+            rectangle(histImg,Point(hue*scale,saturation*scale),
+                      Point((hue+1)*scale-1,(saturation+1)*scale-1),
+                    Scalar::all(intensity),FILLED);
+        }
+
+    imshow("ori",image);
+    imshow("H-S",histImg);
+
+    histone(image);
+
+    waitKey(0);
+
+    return 0;
+}
+
+void histone(Mat& image){
+
+    MatND dstHist;
+    int dims =1;
+    float hrange[] ={0,255};
+    const float* ranges[]={hrange};
+    int size = 256;
+    int channel = 1;
+
+    calcHist(&image,1,&channel,Mat(),dstHist,dims,&size,ranges);
+
+    int scale =1;
+
+    Mat dimage(size*scale,size,CV_8U,Scalar(0));
+
+    double minV= 0;
+    double maxV=0;
+
+    minMaxLoc(dstHist,&minV,&maxV,0,0);
+
+    int hpt = saturate_cast<int>(0.9*size);
+    for(int i=0;i<256;i++)
+    {
+        float binV=dstHist.at<float>(i);
+        int realV = saturate_cast<int>(binV*hpt/maxV);
+        rectangle(dimage,Point(i*scale,size-1),Point((i+1)*scale,size-realV),Scalar(255));
+        cout << binV<<endl;
+    }
+    imshow("uuu",dimage);
+
+}
